f98894f7ae6d2c35e102bce0996f9a07100beadc
p0k02gx <prajwal.kabbinale@walmart.com>
2022-07-13 11:49:20 +0530

    created render templates

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index_fn(request):
+    html = "<h1> Welcome to My Home Page </h1>"
+    #return HttpResponse(html)
+
+    return render(request, template_name='index.html')
+
+def next_fn(request):
+    html = "<h1> Welcome to My Next Page </h1>"
+    return render(request, template_name='base/base.html')
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from first_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home/',views.index_fn),
+    path('next_page/',views.next_fn)
 ]
